999a7e1c520bfd6f469a1f49e2824c1724e5464a
Achintha Pallegedara <achinthahirudika@gmail.com>
2023-07-02 20:03:07 +0530

    player and other

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -7,19 +7,19 @@ import { FaPlay } from "react-icons/fa";
 interface ListItemProps {
     image: string;
     name: string;
-    hraf: string;
+    href: string;
 }
 
 const ListItem: React.FC<ListItemProps>= ({
     image,
     name,
-    hraf
+    href
 }) => {
     const router = useRouter();
 
     const onClick = () => {
         //add auth
-        router.push(hraf);
+        router.push(href);
     }
   return (
     <button onClick={onClick} className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
